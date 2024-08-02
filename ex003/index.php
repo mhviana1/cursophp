dfe68baa46cf1c4be092f030093e3c7c9369edce
mhviana1 <mhviana.adm@gmail.com>
2024-08-02 18:02:57 -0300

    first commit

---
+++ ex003/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+<body>
+    <h1>Teste de tipos de primitivos</h1>
+    <?php 
+    $num = 300;
+    echo "O valor da variável é: $num"
+    ?>
+</body>
+</html>
