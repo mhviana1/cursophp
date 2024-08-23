65f2c070d9665e3dc800cb29c7cf16e578a4722c
Mauricio Viana <mhviana@MacBook-Air-de-Mauricio-2.local>
2024-08-22 23:31:02 -0300

    ex004

--- ex003/index.php
+++ ex003/index.php
@@ -8,8 +8,11 @@
 <body>
     <h1>Teste de tipos de primitivos</h1>
     <?php 
-    $num = 300;
-    echo "O valor da variável é: $num"
+    // $num = 300;
+    // echo "O valor da variável é: $num"
+
+    $vet = [4, 5, 12, 300]
+    echo = "O numero dentro do array é $vet[1]"
     ?>
 </body>
 </html>
