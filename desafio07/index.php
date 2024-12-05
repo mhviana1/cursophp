ffa4781559de21d81c8220e1dbf5919bed94e9eb
Mauricio Viana <mhviana@MacBook-Air-de-Mauricio-2.local>
2024-12-04 22:54:12 -0300

    att v.01.2 desafio007

--- desafio07/index.php
+++ desafio07/index.php
@@ -7,14 +7,27 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <?php
+    $salarioMinimo = 1_412.00;
+    $salario = $_GET["salario"] ?? 0;
+    $quociente = intdiv($salario, $salarioMinimo);
+    $resto = $salario % $salarioMinimo;
+    ?>
     <main>
         <h1>Informe seu salário</h1>
-        <form action="" method="get">
-            <label for="salario"> Salario (R$)</label>
-            <input type="number" name="salario" min="1412">
-            <p>Considerando o salário minimo de <strong>R$ 1.412,00</strong> </p>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            <label for="salario"> Salário (R$)</label>
+            <input type="number" name="salario" id="salario" min="1412">
+            <p>Considerando o salário minimo de <strong>R$<?=number_format($salarioMinimo, 2, ",", ".")?></strong> </p>
             <input type="submit" value="Calcular">
-
-    </main>    
+        </form>    
+    </main>
+    <section>
+        <?php
+        
+        ?>
+        <h2>Resultado Final</h2>
+        <p>Quem recebe um salário de R$<?=number_format($salario, 2, ",", ".")?> ganha <?=$quociente?> salários mínimos + R$<?=number_format($resto, 2, ",", ".")?></p>
+    </section>    
 </body>
 </html>
